f2c3ffdb2afb921407c054ddb8a3de3dbae8e8da
Barbara Gutierrez <ba.gutierrezv30@gmail.com>
2022-12-05 20:48:26 -0300

    Se agrega formulario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
-$(function(){
+$(function () {
     $('[data-bs-toggle="tooltip"]');
 });
 
-$(document).scroll(function(){
+$(document).scroll(function () {
     const y = $("html").scrollTop();
-    
+
     y > 300 ? $("nav").addClass("nav-gris") : $("nav").removeClass("nav-gris")
-    
-    
-    })
+
+
+})
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
