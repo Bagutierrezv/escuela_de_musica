85dca372a62d7563a64192fd66cb1790ca1c7a3b
Barbara Gutierrez <ba.gutierrezv30@gmail.com>
2022-12-06 15:00:37 -0300

    se modifican cards, inicio, footer y carrusel

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,8 @@ $(function () {
 
 $(document).scroll(function () {
     const y = $("html").scrollTop();
-
-    y > 300 ? $("nav").addClass("nav-gris") : $("nav").removeClass("nav-gris")
-
-
+    y > 300 ? $("nav").addClass("nav-gris") : $("nav").removeClass("nav-gris");
 })
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
